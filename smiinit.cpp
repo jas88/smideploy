1029687622059c2000bbc507cf84d483ca4a093f
James A Sutherland <j@sutherland.pw>
2021-02-24 10:19:31 -0600

    Big refactoring, better string handling, sigwait instead of poll and FDs

--- smiinit.cpp
+++ smiinit.cpp
@@ -15,54 +15,26 @@
 #include <map>
 #include <vector>
 
-static int debug = 0, shuttingdown = 0;
+static int debug = 0;
 
 std::vector <pid_t> kids;
-std::map <pid_t, const char *> jobs;
+std::map <pid_t, char * const> jobs;
 
 static void shutdown();
 
-static pid_t spawn(const char *cmd, const char **env);
-
-static int rpipe, wpipe;
-
-static_assert(sizeof(pid_t) == sizeof(int), "Size mismatch!");
-
-static void sig_h(int sig) {
-    switch (sig) {
-        case SIGCHLD: {
-            pid_t p;
-            if (!shuttingdown) {
-                while ((p = waitpid(-1, NULL, WNOHANG)) > 0) {
-                    write(wpipe, &p, sizeof(p));
-                }
-            }
-            return;
-        }
-
-        case SIGTERM: {
-            int n = -1;
-            shuttingdown = 1;
-            if (write(wpipe, &n, sizeof(n)) != sizeof(n)) {
-                perror("write");
-                exit(EXIT_FAILURE);
-            }
-            return;
-        }
-
-        default:
-            fprintf(stderr, "WARNING:Unexpected signal %d\n", sig);
-            return;
-    }
-}
+static pid_t spawn(char * const cmd, const char **env);
 
 static void shutdown() {
-    for (pid_t kid : kids) {
-        jobs.erase(kid);
-        if (kill(kid, SIGTERM)) {
-            fprintf(stderr, "Error %d sending SIGTERM to PID %d\n", errno, kid);
+    auto it = jobs.begin();
+    while (it != jobs.end()) {
+        if (kill(it->first, SIGTERM)) {
+            fprintf(stderr, "Error %d sending SIGTERM to PID %d (%s)\n",
+                    errno, it->first, it->second);
+        free(it->second);
         }
+        it++;
     }
+    jobs.clear();
 
     sleep(3);
 
@@ -76,7 +48,7 @@ static void shutdown() {
     kids.clear();
 }
 
-static pid_t spawn(const char *cmd, const char **env) {
+static pid_t spawn(char * cmd, const char **env) {
     static const char *shell = "/bin/sh", *dashc = "-c";
     pid_t kid;
     switch (kid = fork()) {
@@ -93,36 +65,14 @@ static pid_t spawn(const char *cmd, const char **env) {
         }
 
         default:
-            jobs.insert(std::pair <pid_t, const char *>(kid, cmd));
+            cmd = strdup(cmd);
+            jobs.insert(std::pair <pid_t, char * const>(kid, cmd));
             return kid;
     }
 }
 
 static int init(const char *conffile, const char **env) {
-    struct sigaction sa = {};
-    int pipefd[2];
     YAML::Node config;
-
-    if (pipe(pipefd)
-        || fcntl(pipefd[0], F_SETFL, O_NONBLOCK)
-        || fcntl(pipefd[1], F_SETFL, O_NONBLOCK)) {
-        perror("pipe");
-        return 1;
-    }
-    rpipe = pipefd[0];
-    wpipe = pipefd[1];
-
-    sa.sa_handler = sig_h;
-    sa.sa_flags = SA_RESTART;
-    if (sigaction(SIGTERM, &sa, 0)) {
-        perror("sigaction(sigterm)");
-        return 1;
-    }
-    sa.sa_flags|=SA_NOCLDSTOP;
-    if (sigaction(SIGCHLD, &sa, 0)) {
-        perror("sigaction(sigchld)");
-        return 1;
-    }
 
     try {
         config = YAML::LoadFile(conffile);
@@ -136,7 +86,7 @@ static int init(const char *conffile, const char **env) {
     }
     for (auto job : config["jobs"]) {
         auto jobstring = job.as<std::string>();
-        const char *s = jobstring.c_str();
+        char *s = strdup(jobstring.c_str());
         if (strcmp(s, "wait") == 0) {
             fprintf(stderr, "Waiting 30 seconds for daemons to initialise\n");
             sleep(30);
@@ -144,6 +94,7 @@ static int init(const char *conffile, const char **env) {
             spawn(s, env);
             fprintf(stderr, "Job: %s\n", s);
         }
+        free(s);
     }
 
     return 0;
@@ -162,27 +113,43 @@ static int version() {
     return 0;
 }
 
-static int loop(const char **env) {
-    int pr, n;
-    struct pollfd polling = {};
-    polling.fd = rpipe;
-    polling.events = POLL_IN;
+static int loop(const char *conffile, const char **env) {
+    int signum;
+    sigset_t mask = {0};
+
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGINT);
+    sigaddset(&mask, SIGTERM);
+    sigaddset(&mask, SIGCHLD);
+    if (sigprocmask(SIG_BLOCK, &mask, NULL) == -1) {
+        perror("sigprocmask");
+        return 1;
+    }
+
+    if (init(conffile, env) > 0)
+        return 1;
+
     while (true) {
-        pr = poll(&polling, 1, -1);
-        if (pr < 0 && errno == EINTR)
-            continue;
-        if (pr != 1 || read(rpipe, &n, sizeof(n)) != sizeof(n)) {
-            perror("read");
+        if (sigwait(&mask, &signum)) {
+            perror("sigwait");
             return 1;
         }
-        if (n == -1) {
+        if (signum != SIGCHLD) {
             shutdown();
             return 0;
         } else {
-            try {
-                spawn(jobs.at(n), env);
-            } catch (std::out_of_range&) {
-                // Unwanted orphan, ignore
+            pid_t n;
+            int status;
+            while ((n = waitpid(-1, &status, WNOHANG)) > 0) {
+                fprintf(stderr, "%d exited with status %d, respawning\n",
+                        n, status);
+                try {
+                    spawn(jobs.at(n), env);
+                    free(jobs.at(n));
+                    jobs.erase(n);
+                } catch (std::out_of_range&) {
+                    // Unwanted orphan, ignore
+                }
             }
         }
     }
@@ -210,7 +177,5 @@ int main(int argc, char * const *argv, const char **env) {
                 break;
         }
     }
-    if (init(conffile, env) > 0)
-        return 1;
-    return loop(env);
+    return loop(conffile, env);
 }
