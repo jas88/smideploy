f8746dc137316944d327cfa588779140d83795b5
James A Sutherland <j.a.sutherland@dundee.ac.uk>
2021-07-01 20:54:55 +0000

    dicom-reprocessor should only be triggered manually not by default

--- dockerbits.sh
+++ dockerbits.sh
@@ -194,7 +194,6 @@ jobs:
 - "/smi/smi mongodb-populator -y /smi.yaml"
 - "/smi/smi cohort-packager -y /smi.yaml"
 - "/smi/smi file-copier -y /smi.yaml"
-- "/smi/smi dicom-reprocessor -y /smi.yaml"
 - "/smi/smi identifier-mapper -y /smi.yaml"
 - "/smi/smi update-values -y /smi.yaml"
 
